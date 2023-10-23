94e9bea3b7006fe3745168d28908cfe77f4f329a
foo <foo@bar.com>
2023-10-23 16:31:11 +0800

    files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::env;
 use std::fs;
 use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::{
-    net::{TcpListener, TcpStream},
-};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -13,17 +11,21 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
-        // match stream {
-        //     Ok(stream) => {
-        //         println!("new connection: {}", stream.peer_addr().unwrap());
-        //         handle_connection(stream);
-        //     }
-        //     Err(e) => {
-        //         println!("error: {}", e);
-        //     }
-        // }
+        match stream {
+            Ok(_stream) => {
+                println!("new connection: {}", _stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_connection(_stream);
+                });
+
+                // handle_connection(_stream);
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
         // multi-threaded
-        thread::spawn(|| handle_connection(stream.unwrap()));
+        // thread::spawn(|| handle_connection(stream.unwrap()));
     }
 }
 fn handle_connection(mut stream: TcpStream) {
