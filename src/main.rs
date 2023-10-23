a73a6937be56fb3961731215478c8da54c26a82d
foo <foo@bar.com>
2023-10-23 11:35:37 +0800

    Response 404 stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{Read, Write},
+    io::{BufRead, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -27,13 +27,48 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let response = "HTTP/1.1 200 OK\r\n\r\n";
     stream.write_all(response.as_bytes()).unwrap();
-    let mut buffer = [0; 1024];
-    let bytes_read = stream
-        .read(&mut buffer)
-        .expect("Failed to read from connection");
+    // let mut buffer = [0; 1024];
+    // let bytes_read = stream
+    //     .read(&mut buffer)
+    //     .expect("Failed to read from connection");
 
-    println!(
-        "Received : {}",
-        String::from_utf8_lossy(&buffer[..bytes_read])
-    );
+    // println!(
+    //     "Received : {}",
+    //     String::from_utf8_lossy(&buffer[..bytes_read])
+    // );
+    let mut reader = std::io::BufReader::new(&stream);
+    let mut lines: Vec<String> = Vec::new();
+
+    loop {
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer).unwrap();
+        let buffer = buffer.trim().to_owned();
+        if buffer.is_empty() {
+            break;
+        }
+        lines.push(buffer);
+    }
+    let first_line = lines.first().unwrap();
+
+    let path = get_path(first_line);
+
+    match path.as_str() {
+        "/" => {
+            let response = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+        _ => {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
+}
+
+fn get_path(first_line: &String) -> String {
+    for (i, val) in first_line.split_whitespace().enumerate() {
+        if i == 1 {
+            return val.to_string();
+        }
+    }
+    "".to_string()
 }
