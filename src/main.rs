5ed6322f99dc5b5ebd854593cd2c7c2ad1fa2772
foo <foo@bar.com>
2023-10-23 13:49:15 +0800

    Response content  stage

--- src/main.rs
+++ src/main.rs
@@ -50,36 +50,88 @@ fn handle_connection(mut stream: TcpStream) {
     }
     let first_line = lines.first().unwrap();
 
-    let path = get_path(first_line);
-    eprintln!("path: {}", path);
-    match path.as_str() {
-        "/" => {
-            let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // let path = get_path(first_line);
+    let (_method, uri, _version) = parse_request_line(first_line);
+    // eprintln!("path: {}", path);
+    // match path.as_str() {
+    //     "/" => {
+    //         let response = "HTTP/1.1 200 OK\r\n\r\n";
+    //         stream.write_all(response.as_bytes()).unwrap();
+    //     }
+    //     "/echo" => {
+    //         let length = path.rsplit_once('/').unwrap().0.len();
+    //         let content = path.rsplit_once('/').unwrap().0;
+    //         let response = "HTTP/1.1 200 OK\r\n";
+    //         stream.write_all(response.as_bytes()).unwrap();
+    //         let content_type = "Content-Type: text/plain\r\n";
+    //         let content_length = format!("Content-Length: {}\r\n\r\n", length);
+    //         stream.write_all(content_type.as_bytes()).unwrap();
+    //         stream.write_all(content_length.as_bytes()).unwrap();
+    //         stream.write_all(content.as_bytes()).unwrap();
+    //     }
+    //     _ => {
+    //         let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    //         stream.write_all(response.as_bytes()).unwrap();
+    //     }
+    // }
+    match route(uri) {
+        Some(c) => {
+            let response = generate_response(&c);
             stream.write_all(response.as_bytes()).unwrap();
         }
-        "/echo" => {
-            let length = path.rsplit_once('/').unwrap().0.len();
-            let content = path.rsplit_once('/').unwrap().0;
-            let response = "HTTP/1.1 200 OK\r\n";
-            stream.write_all(response.as_bytes()).unwrap();
-            let content_type = "Content-Type: text/plain\r\n";
-            let content_length = format!("Content-Length: {}\r\n\r\n", length);
-            stream.write_all(content_type.as_bytes()).unwrap();
-            stream.write_all(content_length.as_bytes()).unwrap();
-            stream.write_all(content.as_bytes()).unwrap();
-        }
-        _ => {
+        None => {
             let response = "HTTP/1.1 404 Not Found\r\n\r\n";
             stream.write_all(response.as_bytes()).unwrap();
         }
     }
 }
+//
+//fn get_path(first_line: &String) -> String {
+//    for (i, val) in first_line.split_whitespace().enumerate() {
+//        if i == 1 {
+//            return val.to_string();
+//        }
+//    }
+//    "".to_string()
+//}
 
-fn get_path(first_line: &String) -> String {
+fn parse_request_line(first_line: &String) -> (String, String, String) {
+    let mut method = String::new();
+    let mut uri = String::new();
+    let mut version = String::new();
     for (i, val) in first_line.split_whitespace().enumerate() {
-        if i == 1 {
-            return val.to_string();
+        match i {
+            0 => method = val.to_string(),
+            1 => uri = val.to_string(),
+            2 => version = val.to_string(),
+            _ => {}
         }
     }
-    "".to_string()
+    (method, uri, version)
+}
+
+fn route(uri: String) -> Option<String> {
+    let sections = uri.split('/').collect::<Vec<&str>>();
+    println!("{:?}", sections);
+    if sections.len() < 2 {
+        return None;
+    }
+    if let Some(v) = sections.get(1) {
+        if *v == "echo" {
+            return Some(sections[2..].join("/"));
+        }
+        if v.is_empty() {
+            return Some(String::from(""));
+        }
+    }
+
+    None
+}
+
+fn generate_response(c: &str) -> String {
+    format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+        c.len(),
+        c
+    )
 }
